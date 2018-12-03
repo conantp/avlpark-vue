c554b3368544023ebf2b0076aea75e9cc40ef327
Patrick Conant <conantp@prcappss-mbp.lan>
2018-12-02 20:50:33 -0500

    Remove donate

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,13 @@
     <router-view />
     <div class="prc-footer">
       <div>
-        <a
+        <!-- <a
           href="https://donorbox.org/avlpark"
           class="custom-dbox-popup"
         >
           Donate
         </a>
-        |
+        | -->
         <a href="https://github.com/conantp/avlpark-vue">GitHub</a> |
         <a href="https://data.avlpark.com">Data</a>
       </div>
